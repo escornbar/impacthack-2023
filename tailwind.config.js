155b1b64ce3d3dd5bbdbf1540bb2989043181955
Syaamil Faiq <syaamil1136@gmail.com>
2023-06-17 18:46:24 +0800

    fix success text colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
         },
         success: {
           DEFAULT: "hsl(var(--success))",
-          foreground: "hsl(var(--success))",
+          foreground: "hsl(var(--success-foreground))",
         },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
