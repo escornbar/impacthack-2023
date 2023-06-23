c4653a9ad1a1cd825d61fe43a8937f5ecb8b130f
xinle1030 <xinlelam1030@gmail.com>
2023-06-24 02:47:45 +0800

    remove unused imports

--- backend/src/main/java/com/impacthack/scf/orderItem/OrderItemController.java
+++ backend/src/main/java/com/impacthack/scf/orderItem/OrderItemController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.impacthack.scf.orderTracking.OrderTracking;
 import com.impacthack.scf.product.Product;
 import com.impacthack.scf.product.ProductRepository;
 import com.impacthack.scf.purchaseOrder.PurchaseOrder;
