77f96cbcf447960b33981d89a8b93ecdf9770acd
xinle1030 <xinlelam1030@gmail.com>
2023-06-24 02:47:18 +0800

    Complete Loan Type APIs

---
+++ backend/src/main/java/com/impacthack/scf/loanType/LoanTypeRepository.java
@@ -0,0 +1,7 @@
+package com.impacthack.scf.loanType;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoanTypeRepository extends JpaRepository<LoanType, Long> {
+
+}
