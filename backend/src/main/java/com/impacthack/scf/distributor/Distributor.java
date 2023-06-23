f1ee7b560ba2d671c795757f9b038b32f796cea8
xinle1030 <xinlelam1030@gmail.com>
2023-06-24 02:46:42 +0800

    Make Supplier and Distributor to inherit from Company

--- backend/src/main/java/com/impacthack/scf/distributor/Distributor.java
+++ backend/src/main/java/com/impacthack/scf/distributor/Distributor.java
@@ -2,83 +2,22 @@ package com.impacthack.scf.distributor;
 
 import jakarta.persistence.*;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.impacthack.scf.company.Company;
 
 @JsonIgnoreProperties({"hibernateLazyInitializer"})
 @Entity
 @Table(name = "distributors")
-public class Distributor {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private long distributorId;
-
-  @Column(name = "name")
-  private String name;
-
-  @Column(name="address")
-  private String address;
-
-  @Column(name="contact_name")
-  private String contactName;
-
-  @Column(name="contact_no")
-  private String contactNo;
+@AttributeOverride(name = "name", column = @Column(name = "name"))
+@AttributeOverride(name = "address", column = @Column(name = "address"))
+@AttributeOverride(name = "contactName", column = @Column(name = "contact_name"))
+@AttributeOverride(name = "contactNo", column = @Column(name = "contact_no"))
+public class Distributor extends Company{
 
   public Distributor() {
-
+    super();
   }
 
   public Distributor(String name, String address, String contactName, String contactNo) {
-    this.name = name;
-    this.address = address;
-    this.contactName = contactName;
-    this.contactNo = contactNo;
-  }
-
-  public long getId() {
-    return distributorId;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getContactName() {
-    return contactName;
-  }
-
-  public void setContactName(String contactName) {
-    this.contactName = contactName;
-  }
-
-  public String getAddress() {
-    return address;
-  }
-
-  public void setAddress(String address) {
-    this.address = address;
-  }
-
-  public String getContactNo() {
-    return contactNo;
-  }
-
-  public void setContactNo(String contactNo) {
-    this.contactNo = contactNo;
-  }
-
-  @java.lang.Override
-  public java.lang.String toString() {
-    return "Distributor{" +
-            "distributorId=" + distributorId +
-            ", name='" + name + '\'' +
-            ", address='" + address + '\'' +
-            ", contactName='" + contactName + '\'' +
-            ", contactNo='" + contactNo + '\'' +
-            '}';
+    super(name, address, contactName, contactNo);
   }
 }
--- backend/src/main/java/com/impacthack/scf/supplier/Supplier.java
+++ backend/src/main/java/com/impacthack/scf/supplier/Supplier.java
@@ -2,83 +2,22 @@ package com.impacthack.scf.supplier;
 
 import jakarta.persistence.*;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.impacthack.scf.company.Company;
 
 @JsonIgnoreProperties({"hibernateLazyInitializer"})
 @Entity
 @Table(name = "suppliers")
-public class Supplier {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private long supplierId;
-
-  @Column(name = "name")
-  private String name;
-
-  @Column(name="address")
-  private String address;
-
-  @Column(name="contact_name")
-  private String contactName;
-
-  @Column(name="contact_no")
-  private String contactNo;
+@AttributeOverride(name = "name", column = @Column(name = "name"))
+@AttributeOverride(name = "address", column = @Column(name = "address"))
+@AttributeOverride(name = "contactName", column = @Column(name = "contact_name"))
+@AttributeOverride(name = "contactNo", column = @Column(name = "contact_no"))
+public class Supplier extends Company{
 
   public Supplier() {
-
+    super();
   }
 
   public Supplier(String name, String address, String contactName, String contactNo) {
-    this.name = name;
-    this.address = address;
-    this.contactName = contactName;
-    this.contactNo = contactNo;
-  }
-
-  public long getId() {
-    return supplierId;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getContactName() {
-    return contactName;
-  }
-
-  public void setContactName(String contactName) {
-    this.contactName = contactName;
-  }
-
-  public String getAddress() {
-    return address;
-  }
-
-  public void setAddress(String address) {
-    this.address = address;
-  }
-
-  public String getContactNo() {
-    return contactNo;
-  }
-
-  public void setContactNo(String contactNo) {
-    this.contactNo = contactNo;
-  }
-
-  @java.lang.Override
-  public java.lang.String toString() {
-    return "Supplier{" +
-            "supplierId=" + supplierId +
-            ", name='" + name + '\'' +
-            ", address='" + address + '\'' +
-            ", contactName='" + contactName + '\'' +
-            ", contactNo='" + contactNo + '\'' +
-            '}';
+    super(name, address, contactName, contactNo);
   }
 }
