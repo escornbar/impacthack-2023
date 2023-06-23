e9ea5601566c16640d129c8ffca59572b3872cb1
alifmazli <alifmazli01@gmail.com>
2023-06-24 05:22:10 +0800

    commit

--- frontend/app/orders/page.tsx
+++ frontend/app/orders/page.tsx
@@ -1,5 +1,3 @@
-
-
 import { useEffect, useState } from "react"
 import { Loader2 } from "lucide-react"
 
@@ -130,19 +128,18 @@ export default async function PurchaseOrder() {
   const data = await getData()
   return (
     <>
-
-        <div className="flex-col md:flex">
-          <div className="flex-1 space-y-4 py-8 pt-6">
-            <div className="flex items-center justify-between space-y-2">
-              <h2 className="text-3xl font-bold tracking-tight">
-                Purchase Orders
-              </h2>
-            </div>
-          </div>
-          <div className="flex items-center justify-between space-y-2 overflow-x-auto">
-            <DataTable columns={columns} data={data} />
+      <div className="flex-col md:flex">
+        <div className="flex-1 space-y-4 py-8 pt-6">
+          <div className="flex items-center justify-between space-y-2">
+            <h2 className="text-3xl font-bold tracking-tight">
+              Purchase Orders
+            </h2>
           </div>
         </div>
+        <div className="flex items-center justify-between space-y-2 overflow-x-auto">
+          <DataTable columns={columns} data={data} />
+        </div>
+      </div>
     </>
   )
 }
